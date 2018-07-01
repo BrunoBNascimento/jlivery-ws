c7c33df714ab12e3bbd9edd4cc8bd77a5273d4bb
CaioRodrigues <caio.rod18@gmail.com>
2018-07-01 14:07:26 -0300

    product & restaurant controllers

---
+++ src/controller/RestaurantController.ts
@@ -0,0 +1,21 @@
+import {getRepository} from "typeorm";
+import {NextFunction, Request, Response} from "express";
+import {Restaurant} from "../entity/Restaurant";
+
+export class RestaurantController {
+
+    private restaurantRepository = getRepository(Restaurant);
+
+    async all(request: Request, response: Response, next: NextFunction) {
+        return this.restaurantRepository.find();
+    }
+
+    async one(request: Request, response: Response, next: NextFunction) {
+        return this.restaurantRepository.findOne(request.params.id);
+    }
+
+    async save(request: Request, response: Response, next: NextFunction) {
+        return this.restaurantRepository.save(request.body);
+    }
+
+}
