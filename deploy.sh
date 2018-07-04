776778d799057116efe9bcbe56768e7a512bfe40
Bruno Benicio Do Nascimento <bruno.nascimento@zup.com.br>
2018-07-04 00:12:45 -0300

    jenkins tests

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ rm -rf jlivery-ws && \
 git clone https://BrunoBNascimento:i34dbkub@github.com/BrunoBNascimento/jlivery-ws -v && \
 cd jlivery-ws && \
 ls -a && \
-#docker rm -f jliveryws_jliveryws_1 && \
-#docker rm -f jliveryws_mariadb_1 && \
+docker rm -f jliveryws_jliveryws_1 && \
+docker rm -f jliveryws_mariadb_1 && \
 docker-compose up -d
 
